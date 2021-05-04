2759a71903026269df2690b595b19ff203547bd1
root <root@NetSPI-1103.localdomain>
2021-05-04 02:26:58 -0500

    Remove subscription choice debug code

--- REST/Get-AzDomainInfoREST.ps1
+++ REST/Get-AzDomainInfoREST.ps1
@@ -1,4 +1,4 @@
-﻿#Offload nextLink parsing to a separate function to avoid bloat    
+﻿#Offload nextLink parsing to a separate function to avoid bloat
 Function Get-RESTReq {
     [CmdletBinding()]
     Param(
@@ -8,7 +8,7 @@ Function Get-RESTReq {
         [Parameter(Mandatory=$true)]
         [string]$resourceURI
     )
-    
+
     $runningList = @()
 
     $list = ((Invoke-WebRequest -Uri $resourceURI -Verbose:$false -Method GET -Headers @{ Authorization ="Bearer $managementToken"} -UseBasicParsing).content | ConvertFrom-Json)
@@ -41,7 +41,7 @@ Function Get-AzDomainInfoREST {
         HelpMessage="Dump list of Storage Accounts.")]
         [ValidateSet("Y","N")]
         [String]$StorageAccounts = "Y",
-        
+
         [parameter(Mandatory=$false,
         HelpMessage="Dump list of Resources.")]
         [ValidateSet("Y","N")]
@@ -51,7 +51,7 @@ Function Get-AzDomainInfoREST {
         HelpMessage="Dump list of Virtual Machines.")]
         [ValidateSet("Y","N")]
         [String]$VMs = "Y",
-        
+
         [parameter(Mandatory=$false,
         HelpMessage="Dump list of Network Information.")]
         [ValidateSet("Y","N")]
@@ -70,7 +70,6 @@ Function Get-AzDomainInfoREST {
         $subscriptions = ((Invoke-WebRequest -Uri ('https://management.azure.com/subscriptions?api-version=2019-11-01') -Verbose:$false -Method GET -Headers @{ Authorization ="Bearer $managementToken"} -UseBasicParsing).content | ConvertFrom-Json).value
         # Select which subscriptions to dump info for
         $subChoice = $subscriptions | out-gridview -Title "Select One or More Subscriptions" -PassThru
-        $subChoice = $subscriptions[0]
         if($subChoice.count -eq 0){Write-Verbose 'No subscriptions selected, exiting'; break}
         $SubscriptionId = $subChoice.subscriptionId
 
@@ -78,7 +77,7 @@ Function Get-AzDomainInfoREST {
     else{$subChoice = $SubscriptionId; $noLoop = 1}
 
     Write-Verbose "Dumping information for subscription $SubscriptionID"
-    
+
 
     # Folder Parameter Checking - Creates Az folder to separate from MSOL folder
     if ($folder){
@@ -115,21 +114,21 @@ Function Get-AzDomainInfoREST {
             $strgName = $account.name
 
             Write-Verbose "`tGetting containers for $strgName storage account"
-            
+
             #Get list of containers and iterate through them
             $containers = Get-RESTReq -managementToken $managementToken -resourceURI (-join ("https://management.azure.com", $account.id,"/blobservices/default/containers?api-version=2021-01-01"))
             foreach($container in $containers){
                 $pathName = "\Files\"+$strgName+"\Blob_Files_"+$container.Name
-                
+
                 #Check if the container is public and write it to a file
                 if($container.properties.publicAccess -eq "Container"){
                     $containerName = $container.name
                     $blobUrl = (-join ("https://$strgName.blob.core.windows.net/$containerName/"))
-                    Write-Verbose "`t`t$containerName container is Public" 
+                    Write-Verbose "`t`t$containerName container is Public"
                     $blobUrl >> $folder"\Files\ContainersFileUrls.txt"
                 }
-            } 
-            
+            }
+
             #Enumerate through other storage resources
             $shares = Get-RESTReq -managementToken $managementToken -resourceURI (-join ("https://management.azure.com", $account.id,"/fileServices/default/shares?api-version=2021-01-01"))
             foreach($share in $shares){
@@ -147,15 +146,15 @@ Function Get-AzDomainInfoREST {
         $storageCount = $StorageACCTs.count
         Write-Verbose "`t$storageCount storage accounts were found."
     }
-    
-    
+
+
     if($Resources -eq "Y"){
-        
+
     if(Test-Path $folder"\Resources"){}
     else{New-Item -ItemType Directory $folder"\Resources\" | Out-Null}
 
     Write-Verbose "Getting AzureSQL Resources..."
-    
+
     $sqlServers = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Sql/servers?api-version=2020-08-01-preview"))
     Write-Output $sqlServers
     $count = $sqlServers.count
@@ -165,38 +164,38 @@ Function Get-AzDomainInfoREST {
     #    $databases = (((Invoke-WebRequest -Uri (-join ('https://management.azure.com',$server.id,'/databases?api-version=2020-08-01-preview')) -Verbose:$false -Method GET -Headers @{ Authorization ="Bearer $managementToken"} -UseBasicParsing).Content) | ConvertFrom-Json).value
     #}
 
-    
+
     Write-Verbose "Getting Azure App Services..."
     $appServices = Get-RESTReq -managementToken $managementToken -resourceURI (-join ("https://management.azure.com/subscriptions/",$SubscriptionId,"/providers/Microsoft.Web/sites?api-version=2019-08-01"))
     foreach($app in $appServices){
-       $app.properties | select State,@{name="HostNames";expression={$_.HostNames}},RepositorySiteName,UsageState,Enabled,@{name="EnabledHostNames";expression={$_.EnabledHostNames}},AvailabilityState,@{name="HostNameSslStates";expression={$_.hostNameSslStates}},ServerFarmId,Reserved,LastModifiedTimeUtc,SiteConfig,TrafficManagerHostNames,ScmSiteAlsoStopped,targetSwapSlot,hostingEnvironmentProfile,ClientAffinityEnabled,ClientCertEnabled,HostNamesDisabled,OutboundIpAddresses,PossibleOutboundIpAddresses,ContainerSize,DailyMemoryTimeQuota,SuspendedTill,MaxNumberOfWorkers,CloningInfo,SnapshotInfo,ResourceGroup,IsDefaultContainer,DefaultHostName,SlotSwapStatus,HttpsOnly,Identity,@{name="ID";expression={$app.id}},@{name="Name";expression={$app.name}},@{name="Kind";expression={$app.kind}},@{name="Location";expression={$app.location}},@{name="Type";expression={$app.type}},@{name="Tags";expression={$app.tags}} | Export-Csv -NoTypeInformation -LiteralPath $folder"\Resources\AppServices.CSV" 
+       $app.properties | select State,@{name="HostNames";expression={$_.HostNames}},RepositorySiteName,UsageState,Enabled,@{name="EnabledHostNames";expression={$_.EnabledHostNames}},AvailabilityState,@{name="HostNameSslStates";expression={$_.hostNameSslStates}},ServerFarmId,Reserved,LastModifiedTimeUtc,SiteConfig,TrafficManagerHostNames,ScmSiteAlsoStopped,targetSwapSlot,hostingEnvironmentProfile,ClientAffinityEnabled,ClientCertEnabled,HostNamesDisabled,OutboundIpAddresses,PossibleOutboundIpAddresses,ContainerSize,DailyMemoryTimeQuota,SuspendedTill,MaxNumberOfWorkers,CloningInfo,SnapshotInfo,ResourceGroup,IsDefaultContainer,DefaultHostName,SlotSwapStatus,HttpsOnly,Identity,@{name="ID";expression={$app.id}},@{name="Name";expression={$app.name}},@{name="Kind";expression={$app.kind}},@{name="Location";expression={$app.location}},@{name="Type";expression={$app.type}},@{name="Tags";expression={$app.tags}} | Export-Csv -NoTypeInformation -LiteralPath $folder"\Resources\AppServices.CSV"
     }
     $count = $appServices.count
     Write-Verbose "`t$count App Services enumerated"
-    
-    
+
+
     Write-Verbose "Getting Azure Disks..."
     $disks = Get-RESTReq -managementToken $managementToken -resourceURI (-join ("https://management.azure.com/subscriptions/",$SubscriptionId,"/providers/Microsoft.Compute/disks?api-version=2020-12-01"))
     $disks | Export-CSV -NoTypeInformation -LiteralPath $folder"\Resources\Disks.CSV"
     $disks | ForEach-Object{if($_.properties.encryption -eq ""){$_.Name | Out-File -LiteralPath $folder"\Resources\Disks-NoEncryption.txt"}}
-    
+
     $diskDT = New-Object System.Data.DataTable("disks")
     $columns = @("ResourceGroupName", "TimeCreated", "OsType", "DiskSizeGB", "DiskSizeBytes", "UniqueId", "ProvisioningState", "DiskIOPSReadWrite", "DiskMBpsReadWrite", "DiskState", "Id", "Name", "Location", "Encryption")
     foreach($col in $columns){$diskDT.Columns.Add($col) | Out-Null}
     foreach($disk in $disks){
-        
+
         $diskProps = $disk.properties
         $diskDT.Rows.Add($disk.name, $diskProps.timeCreated, $diskProps.osType, $diskProps.diskSizeGB, $diskProps.diskSizeBytes, $diskProps.uniqueId, $diskProps.provisioningState, $diskProps.diskIOPSReadWrite, $diskProps.diskMBpsReadWrite, $diskProps.diskState, $disk.id, $disk.name, $disk.location, $diskProps.encryption) | Out-Null
     }
 
-    $diskDT | Export-Csv -NoTypeInformation -LiteralPath $folder"\Resources\Disks.csv"    
+    $diskDT | Export-Csv -NoTypeInformation -LiteralPath $folder"\Resources\Disks.csv"
     $count = $disks.count
     Write-Verbose "`t$count Disks were enumerated."
 
     #TODO: Deployments
 
     Write-Verbose "Getting Key Vault Policies..."
-  
+
     $keyVaults = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/resources?`$filter=resourceType eq 'Microsoft.KeyVault/vaults'&api-version=2019-09-01"))
     foreach($vault in $keyVaults){
         $vaultDetails = ((Invoke-WebRequest -Uri (-join ("https://management.azure.com",$vault.id,"?api-version=2019-09-01")) -Verbose:$false -Method GET -Headers @{ Authorization ="Bearer $managementToken"} -UseBasicParsing).content | convertfrom-json)
@@ -216,12 +215,12 @@ Function Get-AzDomainInfoREST {
     #}
 
     Write-Verbose "Getting Automation Account Runbooks and Variables..."
-    
+
     $automationAccounts = Get-RESTReq -managementToken $managementToken -resourceURI (-join ("https://management.azure.com/subscriptions/",$SubscriptionId,"/providers/Microsoft.Automation/automationAccounts?api-version=2015-10-31"))
     if($automationAccounts){
         if(Test-Path $folder"\Resources\AutomationAccounts"){}
         else{New-Item -ItemType Directory $folder"\Resources\AutomationAccounts" | Out-Null}
-    }    
+    }
     foreach($account in $automationAccounts){
         if(Test-Path (-join ($folder,"\Resources\AutomationAccounts\",$account.name))){}
         else{New-Item -ItemType Directory (-join ($folder,"\Resources\AutomationAccounts\",$account.name)) | Out-Null}
@@ -242,15 +241,15 @@ Function Get-AzDomainInfoREST {
     }
     $count = $automationAccounts.count
     Write-Verbose "`t$count Automation Accounts were enumerated."
-    
+
     }
 
     if($NetworkInfo -eq "Y"){
     Write-Verbose "Getting Network Interfaces..."
     if(Test-Path $folder"\Interfaces"){}
     else{New-Item -ItemType Directory $folder"\Interfaces\" | Out-Null}
-   
-    $networkInterfaces = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Network/networkInterfaces?api-version=2020-11-01"))   
+
+    $networkInterfaces = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Network/networkInterfaces?api-version=2020-11-01"))
     foreach($interface in $networkInterfaces){
         $NICName = $interface.name
         foreach($prop in $interface.properties){
@@ -267,7 +266,7 @@ Function Get-AzDomainInfoREST {
     $publicIpTbl.Columns.Add("ResourceGroup") | Out-Null
 
     $publicIps = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Network/publicIPAddresses?api-version=2020-11-01"))
-    
+
     $publicIpCount = 0
     foreach($ip in $publicIps){
         if($ip.properties.ipAddress){
@@ -277,10 +276,10 @@ Function Get-AzDomainInfoREST {
     }
     $publicIpTbl | Export-Csv -NoTypeInformation -LiteralPath $folder"\PublicIPs.csv"
     Write-Verbose("`t$publicIPCount Public IP Addresses were found")
-    
+
     Write-Verbose "`tGetting Network Security Groups..."
     # Create data table to house results
-    $RulesTempTbl = New-Object System.Data.DataTable 
+    $RulesTempTbl = New-Object System.Data.DataTable
     $RulesTempTbl.Columns.Add("NSGName") | Out-Null
     $RulesTempTbl.Columns.Add("ResourceGroupName") | Out-Null
     $RulesTempTbl.Columns.Add("Location") | Out-Null
@@ -315,14 +314,14 @@ Function Get-AzDomainInfoREST {
     Write-Verbose "`t$RulesCount Network Security Group Firewall Rules were enumerated."
     Write-Verbose "`t`t$AnyRulesCount Inbound 'Any Any' Network Security Group Firewall Rules were enumerated."
     }
-    
+
     if($VMs -eq "Y"){
 
 
     # Create folder for VM Info for cleaner output
     if(Test-Path $folder"\VirtualMachines"){}
     else{New-Item -ItemType Directory $folder"\VirtualMachines\" | Out-Null}
-    
+
     $virtualMachines = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Compute/virtualMachines?api-version=2020-06-01"))
     $vmDT = New-Object System.Data.DataTable("vms")
     $columns = @("ResourceGroupName","Name","Location","ProvisioningState")
@@ -343,12 +342,12 @@ Function Get-AzDomainInfoREST {
     if(Test-Path $folder"\RBAC"){}
     else{New-Item -ItemType Directory $folder"\RBAC\" | Out-Null}
 
-    $principalPermissionsTbl = New-Object System.Data.DataTable 
+    $principalPermissionsTbl = New-Object System.Data.DataTable
     $principalPermissionsTbl.Columns.Add("PrincipalID") | Out-Null
     $principalPermissionsTbl.Columns.Add("RoleName") | Out-Null
     $principalPermissionsTbl.Columns.Add("Scope") | Out-Null
 
-    
+
     #Can just grab our identity's principal ID from our JWT
     $tokenPayload = $managementToken.split('.')[1]
     while($tokenPayload.Length % 4){$tokenPayload += "="}
@@ -359,8 +358,8 @@ Function Get-AzDomainInfoREST {
     $rbacAssignments = Get-RESTReq -managementToken $managementToken -resourceURI (-join ('https://management.azure.com/subscriptions/',$SubscriptionId,"/providers/Microsoft.Authorization/roleAssignments?api-version=2015-07-01"))
     foreach($def in $rbacAssignments.properties){
        $roleDefID = $def.roleDefinitionId.split("/")[6]
-       
-        
+
+
         $roleName = ($roleDefinitions | foreach-object {if ($_.name -eq $roleDefID){$_.properties.RoleName}})
         if($roleName){
             $principalPermissionsTbl.Rows.Add($def.principalId, $roleName, $def.scope) | out-null
@@ -384,5 +383,3 @@ Function Get-AzDomainInfoREST {
 
    }
 }
-
-
