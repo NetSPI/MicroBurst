1c5ccbdde19baa77b176e9bc31d233b9c28b7c95
Karl Fosaaen <kfosaaen@gmail.com>
2025-03-19 10:07:06 -0700

    Update MicroBurst.psm1
    Fixed module logic on imports. Function importing was failing if module was already loaded.

--- MicroBurst.psm1
+++ MicroBurst.psm1
@@ -4,45 +4,44 @@ $prefBackup = $WarningPreference
 $global:WarningPreference = 'SilentlyContinue'
 
 # Az
-if(!(Get-Module Az)){
-    try{
-        Import-Module Az -ErrorAction Stop
-        Import-Module $PSScriptRoot\Az\MicroBurst-Az.psm1
-        $azStatus = "1"
-    }
-    catch{Write-Host -ForegroundColor DarkRed "Az module not installed, checking other modules"}
+try{
+    Get-InstalledModule -ErrorAction Stop -Name Az | Out-Null
+    Import-Module Az -ErrorAction Stop
+    Import-Module $PSScriptRoot\Az\MicroBurst-Az.psm1
+    $azStatus = "1"
 }
+catch{Write-Host -ForegroundColor DarkRed "Az module not installed, checking other modules"}
+
 
 
 # AzureAD
-if(!(Get-Module AzureAD)){
-    try{
-        Import-Module AzureAD -ErrorAction Stop
-        Import-Module $PSScriptRoot\AzureAD\MicroBurst-AzureAD.psm1
-    }
-    catch{Write-Host -ForegroundColor DarkRed "AzureAD module not installed, checking other modules"}
+try{
+    Get-InstalledModule -ErrorAction Stop -Name AzureAD | Out-Null
+    Import-Module AzureAD -ErrorAction Stop
+    Import-Module $PSScriptRoot\AzureAD\MicroBurst-AzureAD.psm1
 }
+catch{Write-Host -ForegroundColor DarkRed "AzureAD module not installed, checking other modules"}
 
 <# AzureRm - Uncomment this section if you want to import the functions
-if(!(Get-Module AzureRM)){
-    try{
-        Import-Module AzureRM -ErrorAction Stop
-        Import-Module $PSScriptRoot\AzureRM\MicroBurst-AzureRM.psm1
-    }
-    catch{
-        # If Az is already installed, no need to warn on no AzureRM
-        if($azStatus -ne "1"){Write-Host -ForegroundColor DarkRed "AzureRM module not installed, checking other modules"}
-    }
-}#>
+try{
+    Get-InstalledModule -ErrorAction Stop -Name AzureRM | Out-Null
+    Import-Module AzureRM -ErrorAction Stop
+    Import-Module $PSScriptRoot\AzureRM\MicroBurst-AzureRM.psm1
+}
+catch{
+    # If Az is already installed, no need to warn on no AzureRM
+    if($azStatus -ne "1"){Write-Host -ForegroundColor DarkRed "AzureRM module not installed, checking other modules"}
+}
+#>
 
 # MSOL
-if(!(Get-Module msonline)){
-    try{
-        Import-Module msonline -ErrorAction Stop
-        Import-Module $PSScriptRoot\MSOL\MicroBurst-MSOL.psm1
-    }
-    catch{Write-Host -ForegroundColor DarkRed "MSOnline module not installed, checking other modules"}
+try{
+    Get-InstalledModule -ErrorAction Stop -Name msonline | Out-Null
+    Import-Module msonline -ErrorAction Stop
+    Import-Module $PSScriptRoot\MSOL\MicroBurst-MSOL.psm1
 }
+catch{Write-Host -ForegroundColor DarkRed "MSOnline module not installed, checking other modules"}
+
 
 # Import Additional Functions
 
